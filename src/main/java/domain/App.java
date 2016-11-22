4aa049ace6d064bb1a99e87f493cad55711843bd
Wojciech Iracki <irackiw@gmail.com>
2016-11-22 12:47:44 +0100

    fix connection

--- src/main/java/domain/App.java
+++ src/main/java/domain/App.java
@@ -1,13 +1,44 @@
 package domain;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import dao.AdRepository;
+import dao.MessageRepository;
+import dao.UserRepository;
+import model.User;
+
 public class App 
 {
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/workdb";
+
+	   static final String USER = "root";
+	   static final String PASS = "root";
+	   
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String url = "jdbc:hsqldb:hsql://localhost/workdb";
+    	try {
+			Connection connection = DriverManager.getConnection(url);
+			UserRepository userRepo = new UserRepository(connection);
+			MessageRepository msgRepo = new MessageRepository(connection);
+			AdRepository adRepo = new AdRepository(connection);
+			
+			User janek = new User();
+			janek.setName("Jan");
+			janek.setSurname("Kowalski");
+
+	        System.out.println( "zapisuje janka" );
+			userRepo.add(janek);
+			
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+        System.out.println( "Koniec" );
+        
     }
 }
