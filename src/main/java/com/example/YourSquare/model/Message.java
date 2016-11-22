609d1cd4ae5fa648949250f24e4e4f20fdd17950
Jakub Lebek <s13928@pjwstk.edu.pl>
2016-11-22 11:18:13 +0100

    Added all classes and getters/setters

---
+++ src/main/java/com/example/YourSquare/model/Message.java
@@ -0,0 +1,53 @@
+package com.example.YourSquare.model;
+
+import java.sql.Date;
+
+public class Message {
+	
+	private int message;
+	private String fromUser;
+	private String toUser;
+	private String title;
+	private String content;
+	private Date sendDate;
+	
+	public int getMessage() {
+		return message;
+	}
+	public void setMessage(int message) {
+		this.message = message;
+	}
+	public String getFromUser() {
+		return fromUser;
+	}
+	public void setFromUser(String fromUser) {
+		this.fromUser = fromUser;
+	}
+	public String getToUser() {
+		return toUser;
+	}
+	public void setToUser(String toUser) {
+		this.toUser = toUser;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getSendDate() {
+		return sendDate;
+	}
+	public void setSendDate(Date sendDate) {
+		this.sendDate = sendDate;
+	}
+	
+	
+
+}
--- src/main/java/com/example/YourSquare/model/User.java
+++ src/main/java/com/example/YourSquare/model/User.java
@@ -1,5 +1,107 @@
 package com.example.YourSquare.model;
 
 public class User {
+	
+	private int id;
+	private String name;
+	private String surname;
+	private String adress;
+	private String zipcode;
+	private String city;
+	private String region;
+	private String country;
+	private String phone;
+	private String email;
+	private String password;
+	private String token;
+	private boolean admin;
+	
+	private enum active {
+		Y, N
+	}
+	private enum sex {
+		W, M
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+	
 
 }
